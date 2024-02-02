531cfa99a77c919fc2eb2d7f6795a6cd6543063d
tanya7953 <sharmatanya7953@gmail.com>
2024-02-02 15:20:02 +0530

    Add project files.

---
+++ Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Principal;
+
+namespace PokemonReviewApp.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<PokemonCategory> PokemonCategories { get; set; }
+    }
+}
---
+++ Models/Country.cs
@@ -0,0 +1,11 @@
+﻿namespace PokemonReviewApp.Models
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Owner> Owner { get; set; }
+
+         
+    }
+}
